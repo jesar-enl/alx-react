5240d6cc8874e1e81dc540abfd82c01605ff38da
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-07-24 12:13:29 +0300

    [fix]:edited

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,9 +1,5 @@
 import $ from 'jquery';
 
-$(function () {
-  let p1 = $('<p>').text('Holberton Dashboard');
-  let p2 = $('<p>').text('Dashboard data for the students');
-  let p3 = $('<p>').text('Copyright - Holberton School');
-
-  $('body').append(p1, p2, p3);
-});
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
