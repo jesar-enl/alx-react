0e2ed9cb2873d17478050d7093872dc54628147b
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-08-01 14:21:44 +0300

    lifecycle

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,14 +1,14 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import CourseList from '../CourseList/CourseList';
-import Footer from '../Footer/Footer';
-import Header from '../Header/Header';
-import Login from '../Login/Login';
-import Notification from '../Notifications/Notifications';
-import { getLatestNotification } from '../utils/utils';
+import React from 'react';
 import './App.css';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Notification from '../Notifications/Notifications';
+import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
+import { getLatestNotification } from '../utils/utils';
+import PropTypes from 'prop-types';
 
-class App extends Component {
+class App extends React.Component {
   static listCourses = [
     { id: 1, name: 'ES6', credit: 60 },
     { id: 2, name: 'Webpack', credit: 20 },
@@ -20,7 +20,6 @@ class App extends Component {
     { id: 2, value: 'New resume available', type: 'urgent' },
     { id: 3, html: { __html: getLatestNotification() }, type: 'urgent' },
   ];
-
   constructor(props) {
     super(props);
   }
@@ -31,7 +30,11 @@ class App extends Component {
         <Notification listNotifications={this.listNotifications} />
         <div className="App">
           <Header />
-          {this.props.isLoggedIn ? <CourseList listCourses={this.listCourses} /> : <Login />}
+          {this.props.isLoggedIn ? (
+            <CourseList listCourses={this.listCourses} />
+          ) : (
+            <Login />
+          )}
           <Footer />
         </div>
       </React.Fragment>
